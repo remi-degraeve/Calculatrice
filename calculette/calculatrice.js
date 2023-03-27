0ccaafbcf8f4e1eacf5e9884071c41d96b60a5a6
Rémi.D <90192890+remi-degraeve@users.noreply.github.com>
2023-03-27 18:41:06 +0200

    Update calculatrice.js

--- calculette/calculatrice.js
+++ calculette/calculatrice.js
@@ -1,27 +1,31 @@
-let nombre1 = ''; // variable qui stocke le nombre saisi
-
-
-function affichage(chiffre) {
-    document.getElementById('txt').value += chiffre; //affiche le chiffre du boutton selectionné
-}
-
-function efface() {
-    document.getElementById('txt').value = document.getElementById('txt').value.slice(0, -1); //affiche la même chose sans le dernier élément
-}
-
-function efface1() {
-    document.getElementById('txt').value = ''; // remplace la valeur actuelle par rien
-    nombre1 = '';
-}
-
-
-function calcule1() {
-    nombre1 = document.getElementById('txt').value;
-
-    try {
-        document.getElementById("txt").value = eval(nombre1); // affiche le résultat du calcule
-        nombre1 = '';
-    } catch (error) {
-        window.alert("Une erreur s'est produite : " + error.message); // affiche une erreur si eval(nombre1) n'existe pas
+class BasicCalculator {
+    constructor() {
+      this.nombre1 = ''; // variable qui stocke le nombre saisi
     }
-}
+  
+    affichage(chiffre) {
+      document.getElementById('txt').value += chiffre; //affiche le chiffre du boutton selectionné
+    }
+  
+    efface() {
+      document.getElementById('txt').value = document.getElementById('txt').value.slice(0, -1); //affiche la même chose sans le dernier élément
+    }
+  
+    efface1() {
+      document.getElementById('txt').value = ''; // remplace la valeur actuelle par rien
+      this.nombre1 = '';
+    }
+  
+    calcule1() {
+      this.nombre1 = document.getElementById('txt').value;
+  
+      try {
+          document.getElementById("txt").value = eval(this.nombre1); // affiche le résultat du calcule
+          this.nombre1 = '';
+      } catch (error) {
+          window.alert("Une erreur s'est produite : " + error.message); // affiche une erreur si eval(this.nombre1) n'existe pas
+      }
+    }
+  }
+  
+  const calculator = new BasicCalculator();
